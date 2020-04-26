cf0717aa91cc06accadb7a6af82902301556fc62
Daniel Sasser <danielbsasser@gmail.com>
2020-04-26 12:32:03 +0000

    Automatically backed up by Learn

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -4,4 +4,20 @@ class ArtistsController < ApplicationController
     @artist = Artist.find(params[:id])
   end
 
+  def new
+    @artist = Artist.new
+  end
+
+  def create
+    @artist = Artist.new(artist_params)
+    @artist.save
+    redirect_to artist_path(@artist)
+  end
+
+  private
+
+  def artist_params
+    params.require(:artist).permit(:name, :bio)
+  end
+
 end
